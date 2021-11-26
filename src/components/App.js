2dc56a73182e35d883684538ca54a46d53335616
Wali ullah <waliullah0540@gmail.com>
2021-11-26 12:26:10 +0500

    Updated changes

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ import "./App1.css";
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { tasks: [] };
+    this.state = { tasks: ["Javascript", "Python", "c++"] };
     this.RemoveAll = this.RemoveAll.bind(this);
     this.Pick = this.Pick.bind(this);
     this.addItem = this.addItem.bind(this);
@@ -29,6 +29,7 @@ class App extends React.Component {
     );
   }
 
+  // when component did mount the items will be get back to the state
   componentDidMount() {
     if (!JSON.parse(localStorage.getItem("tasks")).tasks) {
       this.setState({ tasks: [] });
@@ -37,31 +38,41 @@ class App extends React.Component {
       this.setState({ tasks: t.tasks });
     }
   }
+
+  // when component changed the items post to Local Storage
   componentDidUpdate() {
     localStorage.setItem("tasks", JSON.stringify(this.state));
   }
 
+  // function to add item to the list
   addItem(newTask) {
     if (newTask === "") {
       return "Empty is not allowed...";
     } else if (newTask.length < 4) {
-      return "Item should greater than 4 words...";
+      return "Item should be greater than 4 words...";
     } else if (this.state.tasks.indexOf(newTask) >= 0) {
       return "Item already is in the list...";
     }
     this.setState({ tasks: this.state.tasks.concat(newTask) });
   }
 
+  // function to remove all items in list
   RemoveAll() {
     this.setState({ tasks: [] });
   }
 
+  // function for picking one task from state randomly
   Pick() {
     const length = this.state.tasks.length;
     const rand = Math.floor(Math.random() * length);
-    alert(this.state.tasks[rand]);
+    if (this.state.tasks.length <= 0) {
+      alert("Local Storage is empty, Please add some item and try again!");
+    } else {
+      alert(this.state.tasks[rand]);
+    }
   }
 
+  // funtion for cross icon along side with item
   RemoveSingleTask(task) {
     const remainingTasks = this.state.tasks.filter((t) => {
       return t !== task;
